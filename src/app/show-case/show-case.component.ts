ee21f7cd6e47d83b222ed6f9e06b2f63b1228ef9
Valmyr Tavares <valmyrtavares71@gmail.com>
2023-05-29 22:15:21 -0300

    metodos get retirados de dentro do show case component

--- src/app/show-case/show-case.component.ts
+++ src/app/show-case/show-case.component.ts
@@ -37,14 +37,27 @@ export class ShowCaseComponent implements OnInit {
 
   ngOnInit() {
     this.checkCustomer();
-    this.getDataFirebase();
-    this.getData(); // Função para pegar os dados da API ao iniciar o componente
+
+    this.user.getData('portifolio').subscribe((res: any) => {
+      this.showCase = res.filter((item) => item.category === this.id);
+    });
+    this.user.getData('fixedBudget').subscribe((res) => {
+      this.fixedBudget = res.filter((item) => item.category === this.id);
+      this.fixedBudgetObj = {
+        InitialPrice: this.fixedBudget[0].InitialPrice,
+        budgetTitle: this.fixedBudget[0].budgetTitle,
+        category: this.fixedBudget[0].category,
+        deadline: this.fixedBudget[0].deadline,
+        serviceDescription: this.fixedBudget[0].serviceDescription,
+      };
+    });
+    //this.getData(); // Função para pegar os dados da API ao iniciar o componente
     /*Aqui temos um problema que é resolvido com o router.events acima. Toda vez que a rota
     muda ele dispara o getData, porque estamos em uma singlepageAplication e por isso temos 
     essa condição da do event que ao trocar vai trocar também as fotos do evento     
     */
-    this.getDataFirebase();
-    this.getDataFixedBudget();
+
+    //this.getDataFixedBudget();
     // this.getDataExtraItens();
   }
   checkCustomer() {
@@ -53,63 +66,44 @@ export class ShowCaseComponent implements OnInit {
     }
   }
 
-  getData() {
-    //Função para pegar os dados da API
-    this.id = this.route.snapshot.params['id']; //Como pegar a rota e colocar em uma variável
-    this.user.getSocialEvents(this.id).subscribe((res: any) => {
-      const { showCase, fixedBudget, checkBoxList } = res;
-      this.showCase = showCase;
-      //  this.fixedBudget = fixedBudget;
-      this.checkBoxList = checkBoxList;
-    });
-  }
-  getDataFirebase() {
-    this.http
-      .get(
-        'https://projeto-primeiro-de92d-default-rtdb.firebaseio.com/portifolio.json'
-      )
-      .pipe(
-        map((res) => {
-          const showcase = [];
-          for (const key in res) {
-            if (res.hasOwnProperty(key)) {
-              showcase.push({ ...res[key], id: key });
-            }
-          }
-          return showcase;
-        })
-      )
-      .subscribe((res) => {
-        this.showCase = res.filter((item) => item.category === this.id);
-      });
-  }
-  getDataFixedBudget() {
-    this.http
-      .get(
-        'https://projeto-primeiro-de92d-default-rtdb.firebaseio.com/fixedBudget.json'
-      )
-      .pipe(
-        map((res) => {
-          const fixedBudget = [];
-          for (const key in res) {
-            if (res.hasOwnProperty(key)) {
-              fixedBudget.push({ ...res[key], id: key });
-            }
-          }
-          return fixedBudget;
-        })
-      )
-      .subscribe((res) => {
-        this.fixedBudget = res.filter((item) => item.category === this.id);
-        this.fixedBudgetObj = {
-          InitialPrice: this.fixedBudget[0].InitialPrice,
-          budgetTitle: this.fixedBudget[0].budgetTitle,
-          category: this.fixedBudget[0].category,
-          deadline: this.fixedBudget[0].deadline,
-          serviceDescription: this.fixedBudget[0].serviceDescription,
-        };
-      });
-  }
+  // getData() {
+  //   //Função para pegar os dados da API
+  //   this.id = this.route.snapshot.params['id']; //Como pegar a rota e colocar em uma variável
+  //   this.user.getSocialEvents(this.id).subscribe((res: any) => {
+  //     const { showCase, fixedBudget, checkBoxList } = res;
+  //     this.showCase = showCase;
+  //     //  this.fixedBudget = fixedBudget;
+  //     this.checkBoxList = checkBoxList;
+  //   });
+  // }
+
+  // getDataFixedBudget() {
+  //   this.http
+  //     .get(
+  //       'https://projeto-primeiro-de92d-default-rtdb.firebaseio.com/fixedBudget.json'
+  //     )
+  //     .pipe(
+  //       map((res) => {
+  //         const fixedBudget = [];
+  //         for (const key in res) {
+  //           if (res.hasOwnProperty(key)) {
+  //             fixedBudget.push({ ...res[key], id: key });
+  //           }
+  //         }
+  //         return fixedBudget;
+  //       })
+  //     )
+  //     .subscribe((res) => {
+  //       this.fixedBudget = res.filter((item) => item.category === this.id);
+  //       this.fixedBudgetObj = {
+  //         InitialPrice: this.fixedBudget[0].InitialPrice,
+  //         budgetTitle: this.fixedBudget[0].budgetTitle,
+  //         category: this.fixedBudget[0].category,
+  //         deadline: this.fixedBudget[0].deadline,
+  //         serviceDescription: this.fixedBudget[0].serviceDescription,
+  //       };
+  //     });
+  // }
 
   createNewUser(open: boolean) {
     this.knownUser = open;
